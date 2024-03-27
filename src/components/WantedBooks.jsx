4a4a3d232b40c8151f3f4529b675d2dc83b5e21b
Rafidul01 <rafidulhasan555@gmail.com>
2024-03-27 06:58:28 +0600

    read books and wish list card design done

--- src/components/WantedBooks.jsx
+++ src/components/WantedBooks.jsx
@@ -1,17 +1,47 @@
+import { CiLocationOn } from "react-icons/ci";
+import { IoNewspaperOutline } from "react-icons/io5";
+import { IoPeopleSharp } from "react-icons/io5";
 const WantedBooks = () => {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-      <figure>
+    <div className="card lg:card-side bg-base-100 shadow-xl border">
+      <figure className="p-6">
         <img
-          src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+          src="https://i.ibb.co/6WmTmN4/b2-removebg-preview.png"
           alt="Album"
+          className="rounded-2xl lg:w-[230px] lg:h[172px] bg-[#1313130D] "
         />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Listen</button>
+      <div className="p-6 space-y-4 w-auto">
+        <h2 className="card-title playfair-display font-bold text-2xl text-[#131313]">
+          New album is released!
+        </h2>
+        <p className="text-[#131313CC] work-sans font-medium text-base">
+          By :{" "}
+        </p>
+        <div className="flex items-center gap-4">
+          <h1 className="text-[#131313] font-medium text-xl work-sans ">
+            Tag{" "}
+          </h1>
+          <div className=" font-medium text-xl work-sans flex items-center gap-2">
+            <CiLocationOn className="text-[#131313CC]" />
+            <h1 className="text-[#131313CC]">Year of Publishing: 1924</h1>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className=" font-medium text-xl work-sans flex items-center gap-2 ">
+            <IoPeopleSharp className="text-[#13131399]" />
+            <h1 className="text-[#13131399]">Publisher: Scribner</h1>
+          </div>
+          <div className=" font-medium text-xl work-sans flex items-center gap-2">
+            <IoNewspaperOutline className="text-[#13131399]" />
+            <h1 className="text-[#13131399]">Page 192</h1>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col lg:flex-row gap-4">
+          <button className="btn bg-[##328EFF26] text-[#328EFF] rounded-[30px]">Category: Classic</button>
+          <button className="btn bg-[#FFAC3326] text-[#FFAC33] rounded-[30px]">Rating: 4.5</button>
+          <button className="btn bg-[#23BE0A] text-white rounded-[30px]">View Details</button>
         </div>
       </div>
     </div>
--- src/pages/ListedBooks.jsx
+++ src/pages/ListedBooks.jsx
@@ -19,40 +19,6 @@ const ListedBooks = () => {
         </select>
       </div>
       <div>
-        {/* <div role="tablist" className="tabs tabs-lifted">
-        
-          <input
-            type="radio"
-            name="my_tabs_2"
-            role="tab"
-            className="tab text-[#131313CC] font-normal text-base"
-            aria-label="Read Books"
-          />
-         
-          <div
-            role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
-          >
-            <ReadBook></ReadBook>
-          </div>
-
-          <input
-            type="radio"
-            name="my_tabs_2"
-            role="tab"
-            className="tab text-[#131313CC] font-normal text-base"
-            aria-label="Wishlist Books"
-            checked
-          />
-          <div
-            role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
-          >
-            Tab content 2
-          </div>
-
-          
-        </div> */}
         <div role="tablist" className="tabs tabs-lifted flex justify-start mb-4">
           <Link onClick={()=>setTabIdx(0)}  role="tab" to={`readBooks`} className= {`tab font-normal text-base ${tabIdx === 0 ? 'tab-active text-[#131313CC]' : 'text-[#13131380]'} `} >
           Read Books
