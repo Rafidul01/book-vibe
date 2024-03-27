61212b25d5552f44212f98d86154f4407771e58e
Rafidul01 <rafidulhasan555@gmail.com>
2024-03-27 09:02:12 +0600

    wishlist added

--- src/components/WishlistBooks.jsx
+++ src/components/WishlistBooks.jsx
@@ -1,10 +1,32 @@
+import { useLoaderData } from "react-router-dom";
 import WantedBooks from "./WantedBooks";
+import { useEffect, useState } from "react";
+import { getStoredWishBooks } from "../utilitiy/localstrorage";
 
 const WishlistBooks = () => {
-    return (
-        <div>
+    const books = useLoaderData();
+    const [wishBook, setWishBook] = useState([])
+    useEffect(()=>{
+        const storedBookId = getStoredWishBooks();
+        if(books.length > 0){
+            const readdedBooks = [];
+            for(const id of storedBookId){
+                const book = books.find(book => book.id === id );
+                if(book){
+                    readdedBooks.push(book);
+                }
 
-            {/* <WantedBooks></WantedBooks> */}
+            }
+            setWishBook(readdedBooks);
+            
+        }
+    },[])
+    return (
+        <div className="space-y-4">
+            {
+                wishBook.map(book => <WantedBooks key={book.id} book={book} ></WantedBooks>)
+            }
+            
         </div>
     );
 };
